ac1e8fe9798bb667d63d3ae69ae3d76be80aec8e
Ned <escauley@users.noreply.github.com>
2020-11-02 15:49:21 -0500

    Add files via upload

---
+++ quote_csv.py
@@ -0,0 +1,50 @@
+# For editing the csv file.
+import csv
+
+# For taking command line arguments.
+import argparse
+
+# Arguments
+# ---------
+
+# csv_file: the path to the original csv file to edit.
+
+# output_file: the path to the output csv file that will contain the edits. File does not need to already exist.
+
+# Returns
+# -------
+
+# The edited csv file to the path specified in output_file argument.
+
+
+def main(csv_file, output_file):
+
+    # Read the original csv file and load data into a list.
+    with open(csv_file, 'r') as file:
+        original_file = csv.reader(file)
+
+        # Create a list to hold the data.
+        data = []
+
+        # Add to the data list.
+        for row in original_file:
+            data.append(row)
+
+    # Write data list to the new file with quotes around all values.
+    with open(output_file, 'w') as out_file:
+        writer = csv.writer(out_file, quoting=csv.QUOTE_ALL)
+        writer.writerows(data)
+
+    # Output message.
+    print('csv file ' + csv_file + ' written with quotes to ' + output_file)
+
+# Command line arguments.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Commands for mapping script.')
+    parser.add_argument('--csv_file', '-f',
+                        help='The csv file')
+    parser.add_argument('--output_file', '-o',
+                        help='The output file')
+    args = parser.parse_args()
+
+    main(args.csv_file, args.output_file)
