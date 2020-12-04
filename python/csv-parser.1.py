28be2e7c55d55def058055332516033aafd2e0bc
Alex Coleman <73913404+Uncreative-Username-Generator@users.noreply.github.com>
2020-12-04 09:15:51 -0500

    Update csv-parser.1.py

--- python/csv-parser.1.py
+++ python/csv-parser.1.py
@@ -1,10 +1,10 @@
-import os,sys
+import os, sys
 import string
 import json
 from optparse import OptionParser
 import csv
 
-__version__="1.0"
+__version__ = "1.0"
 __status__ = "Dev"
 
 """
@@ -20,31 +20,27 @@ Usage:
 python csv-parser.1.py -i <filename.csv>
 """
 
+
 def main():
+    usage = "\n%prog  [options]"
+    parser = OptionParser(usage, version="%prog " + __version__)
+    parser.add_option("-i", "--infile", action="store", dest="infile", help="Input file")
 
-	usage = "\n%prog  [options]"
-	parser = OptionParser(usage,version="%prog " + __version__)
-	parser.add_option("-i","--infile",action="store",dest="infile",help="Input file")
+    (options, args) = parser.parse_args()
+    for file in ([options.infile]):
+        if not (file):
+            parser.print_help()
+            sys.exit(0)
 
-	(options,args) = parser.parse_args()
-	for file in ([options.infile]):
-		if not (file):
-			parser.print_help()
-			sys.exit(0)
+    inFile = options.infile
 
-	inFile = options.infile
-
-	with open(inFile, 'r') as FR:
-        	dataFrame = csv.reader(FR, delimiter=',', quotechar='"')
-        	rowCount = 0
-		for row in dataFrame:
-			rowCount += 1
-			print (rowCount, row)
-
-
+    with open(inFile, 'r') as FR:
+        dataFrame = csv.reader(FR, delimiter=',', quotechar='"')
+        rowCount = 0
+        for row in dataFrame:
+            rowCount += 1
+            print(rowCount, row)
 
 
 if __name__ == '__main__':
-        main()
-
-
+    main()
