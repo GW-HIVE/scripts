a35b79b5857fed635bb9e57dad7b01cfc52ae3b1
Uncreative-Username-Generator <alexc401101@gmail.com>
2021-01-12 12:24:36 -0500

    Renaming scripts

--- python/sas7bdat_to_csv.py
+++ python/sas7bdat_to_csv.py
@@ -10,14 +10,14 @@ __status__ = "Dev"
 """
 This script will transfer a .sas file to a .csv file.
 This script has three options, you can execute the script in three ways:
-1. python sas7bdatTocsv.1.py --version
+1. python sas7bdat_to_csv.py --version
 This is the option that show you the program's version.
-2. python sas7bdatTocsv.1.py -h
+2. python sas7bdat_to_csv.py -h
 This can show you some help information.
-3. python sas7bdatTocsv.1.py -i <input.sas7bdat> -o <output.csv>
+3. python sas7bdat_to_csv.py -i <input.sas7bdat> -o <output.csv>
 -i specifies input
 Usage:
-python sas7bdatTocsv.1.py -i <input.sas7bdat> -o <output.csv>
+python sas7bdat_to_csv.py -i <input.sas7bdat> -o <output.csv>
 """
 
 
@@ -27,10 +27,12 @@ def main():
     parser = ArgumentParser(description=usage)
     parser.add_argument("-i", "--sasFile", action="store", dest="sasFile", help="Input sas file")
     parser.add_argument("-o", "--csvFile", action="store", dest="csvFile", help="Output csv file")
-    parser.add_argument("-d", "--delim", action="store", dest="delim", help="Optional deliminator for output file. "
-                                                                            "If not provided, default is comma.")
+    parser.add_argument("-d", "--delim", action="store", dest="delim", help="Optional deliminator "
+                                                                            "for output file. "
+                                                                            "If not provided, "
+                                                                            "default is comma.")
 
-    (options, args) = parser.parse_args()
+    options = parser.parse_args()
     for file in ([options.sasFile, options.csvFile]):
         if not file:
             parser.print_help()
@@ -38,16 +40,16 @@ def main():
 
     sas_file = options.sasFile
     out_file = options.csvFile
-    fw = csv.writer(open(out_file, 'wb'))
-    with SAS7BDAT(sas_file) as f:
-        for row in f:
+    file_writer = csv.writer(open(out_file, 'wb'))
+    with SAS7BDAT(sas_file) as file:
+        for row in file:
             line = str(row[0])
             for i in range(1, len(row)):
                 row[i] = str(row[i])
                 if options.delim is not None:
                     row[i] = row[i].replace(",", options.delim)
                 line += ", %s" % (str(row[i]))
-            fw.writerow(line)
+            file_writer.writerow(line)
 
 
 if __name__ == '__main__':
